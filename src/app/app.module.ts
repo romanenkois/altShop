5c22a1831e9b96088bc3aca3b623621df7824946
Illya <romanenkoisnot@gmail.com>
2024-02-04 17:51:59 +0200

    rebuilding app for tailwind to work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
-import { ProductsPageComponent } from './pages/products-page/products-page.component';
-import { Error404PageComponent } from './pages/error404-page/error404-page.component';
 import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
-    ProductsPageComponent,
-    Error404PageComponent,
-    HeaderComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    colors: {
+      primary: '#ffdd00',
+    }
+  },
+  plugins: [],
+}
+
