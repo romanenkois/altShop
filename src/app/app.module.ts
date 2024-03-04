931fc387f965a6eb69df7afba6e2112be217abda
Illya <romanenkoisnot@gmail.com>
2024-03-04 14:56:31 +0200

    small changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomePageComponent } from './pages/home-page/home-page.component';
-import { HeaderComponent } from './components/header/header.component';
 
 import { registerLocaleData } from '@angular/common';
 import uk from '@angular/common/locales/uk';
@@ -22,11 +19,16 @@ import { NzButtonModule } from 'ng-zorro-antd/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatMenuModule} from '@angular/material/menu';
+
+import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ProductsPageComponent } from './pages/products-page/products-page.component';
 import { ProductPageComponent } from './pages/product-page/product-page.component';
 import { Error404PageComponent } from './pages/error404-page/error404-page.component';
-import { ProductFrameComponent } from './components/product-frame/product-frame.component';
 import { CartPageComponent } from './pages/cart-page/cart-page.component';
+
+import { HeaderComponent } from './components/header/header.component';
+import { ProductFrameComponent } from './components/product-frame/product-frame.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 registerLocaleData(uk);
 
@@ -39,7 +41,8 @@ registerLocaleData(uk);
     ProductPageComponent,
     Error404PageComponent,
     ProductFrameComponent,
-    CartPageComponent
+    CartPageComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
